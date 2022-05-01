7e56b6068f9e40015f68384e04330ba11acdd99d
Gaju5-Ndg <87604337+Gaju5-Ndg@users.noreply.github.com>
2022-05-01 13:40:40 +0200

    chenged the port

--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerOptions from './swagger.js';
 // import {} from "dotenv/config";
 import express from 'express';
-import mongoConnect from './config/db.config.js';
+// import mongoConnect from './config/db.config.js';
 import userRouter  from './routes/user.js';
 import postRouter from './routes/blog.js';
 import messageRoutes  from './routes/messages.js';
 import commentRouter from './routes/comments.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -18,12 +19,20 @@ app.use('/api/message',messageRoutes )
 app.use('/api/comment', commentRouter)
 app.use('/api/port', swaggerUi.serve,swaggerUi.setup(swaggerOptions))
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
-app.listen(3000, () => {
-    console.log(`Server Started at ${port}`)
+mongoose.connect('mongodb+srv://Rose-31:ikinyogote@cluster0.ehn4f.mongodb.net/test',{useUnifiedTopology:true}).then(() =>{
+    console.log("database connected successfly");
+    app.listen(port);
+    console.log(`Server Started at ${port}`);
+}).catch(error =>{
+    console.log(error)
 })
 
-   mongoConnect();
+// app.listen(3000, () => {
+//     console.log(`Server Started at ${port}`)
+// })
+
+//    mongoConnect();
 
    export default app
